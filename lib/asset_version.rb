f529f1aec91c66670e8b1a80f4d6db90621e8dc4
Kristopher Chambers <kristopher.chambers@gmail.com>
2008-12-11 19:57:19 -0500

    Dryed up the tag methods.

--- lib/asset_version.rb
+++ lib/asset_version.rb
@@ -1,30 +1,37 @@
 module ActionView::Helpers::AssetTagHelper  
   
   def javascript_include_tag_with_versioned(*sources)
-    options = sources.extract_options!.stringify_keys!
-    options['cache'] = version_cache_name(options['cache'], options.delete('version'))
-    javascript_include_tag_without_versioned(sources, options)
+    set_cache_options_with_versioning_from_sources!(sources)
+    javascript_include_tag_without_versioned(*sources)
+    
   end
 
   def stylesheet_link_tag_with_versioned(*sources)
-    options = sources.extract_options!.stringify_keys!
-    options['cache'] = version_cache_name(options['cache'], options.delete('version'))
-    stylesheet_link_tag_without_versioned(sources, options)
+    set_cache_options_with_versioning_from_sources!(sources)
+    stylesheet_link_tag_without_versioned(*sources)
   end  
   
-  def version_cache_name(cache_name, version)
-    if cache_name
-      cache_name = 'all' unless cache_name.is_a? String
-      cache_name = 
-        if version
-          [cache_name, version].join('_')          
-        else
-          [cache_name, Time.now.to_i].join('_')  
-        end
-    end
-  end
-
   alias_method_chain :javascript_include_tag, :versioned
   alias_method_chain :stylesheet_link_tag, :versioned
+
+  private   
+    def set_cache_options_with_versioning_from_sources!(sources)
+      options = sources.extract_options!.stringify_keys!
+      options['cache'] = version_cache_name(options['cache'], options.delete('version'))
+      sources << options
+    end
+    
+    def version_cache_name(cache_name, version)
+      if cache_name
+        cache_name = 'all' unless cache_name.is_a? String
+        cache_name = 
+          if version
+            [cache_name, version].join('_')          
+          else
+            [cache_name, Time.now.to_i].join('_')  
+          end
+      end
+    end
+
 
 end

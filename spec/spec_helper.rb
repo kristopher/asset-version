21d0a8ad26112571de2cab3c7ba2a57050b94bf9
Kristopher Chambers <kristopher.chambers@gmail.com>
2008-12-12 17:50:40 -0500

    Adding rspec for testing.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'active_support'
+require 'active_support/test_case'
+require 'action_view'
+require 'action_controller'
+
+require File.join(File.dirname(__FILE__), '..', 'lib', 'asset_version')
+
